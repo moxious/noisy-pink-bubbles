53ed15a60fa1a6f560aad3cf740f182f2e6631c7
Moxious <moxious@oldhat.org>
2018-01-01 20:52:46 -0500

    better hexagons

--- src/model/shapes/index.js
+++ src/model/shapes/index.js
@@ -20,12 +20,13 @@ const shapeRegistry = {
     ]),
 
     hexagon: (scale = DEFAULT_SCALE) => ([
-        { x: 0 * scale, y: 1 * scale },
-        { x: 1 * scale, y: 0 * scale },
-        { x: 2 * scale, y: 1 * scale },
-        { x: 2 * scale, y: 2 * scale },
-        { x: 1 * scale, y: 3 * scale },
-        { x: 0 * scale, y: 2 * scale },
+        // https://www.redblobgames.com/grids/hexagons/
+        { x: 0 * scale * 3, y: 1 * scale * 3 },
+        { x: 0.25 * scale * 3, y: 0.5 * scale * 3},
+        { x: 0.75 * scale * 3, y: 0.5 * scale * 3 },
+        { x: 1 * scale * 3,  y: 1 * scale * 3 },
+        { x: 0.75 * scale * 3, y: 1.5 * scale * 3 },
+        { x: 0.25 * scale * 3, y: 1.5 * scale * 3 },
     ]),
 
     square: (scale = DEFAULT_SCALE) => ([
