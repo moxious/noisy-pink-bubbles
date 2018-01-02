6dac8e920ee6b039f5b5a50fa0bf52421956779a
Moxious <moxious@oldhat.org>
2018-01-02 11:46:57 -0500

    more sensible palette choices

--- src/model/Palette.js
+++ src/model/Palette.js
@@ -1,6 +1,22 @@
 import * as palette from 'google-palette';
 
-const schemes = ['sequential', 'tol', 'qualitative'];
+const schemes = ['sequential', 
+    ['tol', 'dv'],
+    ['tol', 'sq'],
+    ['tol', 'rainbow'],
+
+    ['sequential', 'cbf', 'Blues'],
+    ['sequential', 'cbf', 'Reds'],
+
+    ['sequential', 'cbf', 'BuGn'],
+    ['sequential', 'cbf', 'BuPu'],
+    ['sequential', 'cbf', 'GnBu'],
+
+    ['diverging', 'cbf', 'BrBG'],
+    ['diverging', 'cbf', 'PRGn'],
+    ['diverging', 'cbf', 'RdYlBu'],
+    ['diverging', 'cbf', 'Spectral'],
+];
 
 /**
  * Generate a variable sized, dynamic palette, following design principles from 
@@ -9,6 +25,11 @@ const schemes = ['sequential', 'tol', 'qualitative'];
  */
 export default () => {
     const randScheme = schemes[Math.floor(Math.random() * schemes.length)];
-    const p = palette([randScheme], Math.floor(Math.random() * 8) + 5);
+    const p = palette(randScheme, Math.floor(Math.random() * 8) + 5);
+    if (!p) {
+        throw new Error('Invalid palette ' + randScheme);
+    }
+
+    // console.log('Palette ', randScheme, p);
     return p.map(color => `#${color}`);
 }
