ba63a65899afe61759d273367531e6b385ca0a7c
Moxious <moxious@oldhat.org>
2018-01-01 20:53:03 -0500

    dynamic google palettes for the bubbles

--- src/model/Palette.js
+++ src/model/Palette.js
@@ -1,13 +1,15 @@
-const palettes = [
-    // Generated via http://colormind.io/template/paper-dashboard/
-    ['#CAE56A', '#86A220', '#4A8024', '#265A6F', '#071D26'],
-    ['#6AA3E5', '#1A4C86', '#0C28B6', '#411193', '#120726'],
-    ['#E56A87', '#9C1333', '#C32502', '#BC5302', '#261407'],
-    ['#1B264F', '#274690', '#576CA8', '#302B27', '#F5F3F5'],
-    ['#EFE9E1', '#958E80', '#904E20', '#957D53', '#171516'],
-    ['#EAECE8', '#89B6A2', '#A18D51', '$969E9A', '#455870'],
-    ['#F9FBFA', '#AD9D96', '#DDB69C', '#E3857D', '#F36812'],
-    ['#EBF0F0', '#848979', '#88A79B', '#715E5D', '#15324F'],
-];
+import * as palette from 'google-palette';
 
-export default () => palettes[Math.floor(Math.random() * palettes.length)];
+const schemes = ['sequential', 'tol', 'qualitative'];
+
+/**
+ * Generate a variable sized, dynamic palette, following design principles from 
+ * the google people.
+ * https://www.npmjs.com/package/google-palette
+ */
+export default () => {
+    const randScheme = schemes[Math.floor(Math.random() * schemes.length)];
+    const p = palette([randScheme], Math.floor(Math.random() * 8) + 5);
+    console.log('PALETTE ', p);
+    return p.map(color => `#${color}`);
+}
